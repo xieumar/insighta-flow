7cb4715a0d805b7eae45020db58136581e718581
Zainab Umar Ibrahim <xieumar@gmail.com>
2026-06-10 13:37:39 +0100

    feat(upload): add IngestionSummary component and export module barrel

---
+++ frontend/modules/upload/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./hooks/useUpload";
+export * from "./components/DropZone";
+export * from "./components/IngestionSummary";
+export * from "./components/ProgressRing";
+export * from "./components/UploadStatusBar";
